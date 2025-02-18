2e48a71b1aa2826a8c8b71a9b67fd53102f561d2
chimallidev <chimalli.dev@gmail.com>
2025-02-17 19:16:49 -0600

    Logo eliminad reflex init eliminado

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 source venv_chimalli/Scripts/activate
 python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
-reflex init 
 reflex export --frontend-only
 rm -rf public
 unzip frontend.zip -d public
