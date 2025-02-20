7644ed3099ed51f3ddb2a04a5a917054b3c2f883
chimallidev <chimalli.dev@gmail.com>
2025-02-20 17:32:31 -0600

    Cursos page add in Public

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-source venv_chimalli/Scripts/activate
+source .venv_chimalli/Scripts/activate
 python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
 reflex export --frontend-only
