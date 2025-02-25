c9814449615a829edcf1e18fd4dba3f351579a2e
chimallidev <chimalli.dev@gmail.com>
2025-02-24 19:00:40 -0600

    CORS added

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 source .venv_chimalli/Scripts/activate
 python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
-reflex export --frontend-only
+API_URL=https://chimallidev-links-web.up.railway.app/ reflex export --frontend-only
 rm -rf public
 unzip frontend.zip -d public
 rm -f frontend.zip
--- rxconfig.py
+++ rxconfig.py
@@ -2,4 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="links_chimalli",
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://chimallidev-links-web.vercel.app/"
+    ]
 )
